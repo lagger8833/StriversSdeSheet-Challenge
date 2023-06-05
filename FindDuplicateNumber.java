862f19e2197124732c9fd0ed9d74565b1247ff30
Vighneshwar M <57844835+lagger8833@users.noreply.github.com>
2023-06-05 13:25:16 +0530

    Question 2: Find Duplicate Number

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class FindDuplicateNumber
+ {
+   public static void main(String args[])
+   { 
+     List<Integer> arr = new ArrayList<>(Arrays.asList(1,2,6,4,3,2));
+     System.out.print(findDuplicate(arr));
+   }
+ }
