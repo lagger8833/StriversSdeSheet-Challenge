e4a6308002d3399d8077865d9d2c6c9b99d48341
Vighneshwar M <57844835+lagger8833@users.noreply.github.com>
2023-06-05 13:31:11 +0530

    FindDuplicateNumber.java
    Updates function as was unable to upload via phone

--- FindDuplicateNumber.java
+++ FindDuplicateNumber.java
@@ -4,9 +4,23 @@ import java.util.*;
 
  class FindDuplicateNumber
  {
+   public static int findDuplicate(List<Integer> arr){
+     int slow = arr.get(0), fast = arr.get(0);
+     do{
+        slow = arr.get(slow);
+        fast = arr.get(arr.get(fast));
+     }while(slow != fast);
+     fast = arr.get(0);
+     while(slow != fast){
+        slow = arr.get(slow);
+        fast = arr.get(fast);
+     }
+     return slow;
+   }
    public static void main(String args[])
    { 
-     List<Integer> arr = new ArrayList<>(Arrays.asList(1,2,6,4,3,2));
-     System.out.print(findDuplicate(arr));
+      List<Integer> arr = new ArrayList<>(Arrays.asList(1,2,6,4,3,2));
+      System.out.print(findDuplicate(arr));
+    //Output is 2
    }
  }
